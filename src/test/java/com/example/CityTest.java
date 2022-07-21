60f109feabd591530ec2de961b6fec721022dd6c
ciscons <cisconamsu@gmail.com>
2022-07-21 11:21:25 +0900

    프로젝트 완성

--- src/test/java/com/example/CityTest.java
+++ src/test/java/com/example/CityTest.java
@@ -1,8 +1,6 @@
 package com.example;
 
-import java.sql.Connection;
-
-import javax.sql.DataSource;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,25 +8,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.example.service.CityService;
+import com.example.vo.CityVo;
+
 import lombok.extern.java.Log;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration("classpath:beans.xml")
 @Log
 class CityTest {
-	@Autowired
-	private DataSource dataSource;
+
+	@Autowired	
+	private CityService cityService;
 	
 	@Test
-	void test() {
-//		assertNotNull(this.dataSource);
-		try {
-			Connection conn = this.dataSource.getConnection();
-			log.info(conn.toString());
-		} catch (Exception e) {
-			log.warning(e.getMessage());
+	public void test() {
+		assertNotNull(this.cityService);
+	}
+	
+	@Test
+	public void test1() {
+		CityVo city = this.cityService.selectCity("Seoul");
+		log.info(city.toString());
+	}
+	
+	@Test
+	public void test2() {
+		for(CityVo city : this.cityService.selectAllCitiesByCountryCode("KOR")) {
+			log.info(city.toString());
 		}
-//		log.info("Hello World!");
 	}
 
 }
